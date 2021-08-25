578f28ed5fdb2bf2862c5492c83520f68f1b0183
Zarrar Ahmed Khan <zarrar.ahmed@systemsltd.com>
2021-08-25 13:09:36 +0500

    commiting latest code

---
+++ src/test/java/Pages/TestListener.java
@@ -0,0 +1,78 @@
+package Pages;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import io.qameta.allure.Attachment;
+
+
+public class TestListener implements ITestListener {
+
+	public static String getTestMethodName(ITestResult iTestResult){
+   return iTestResult.getMethod().getConstructorOrMethod().getName();
+	}
+	
+	//Text attachments or allure
+	@Attachment(value="Page Screenshot", type="image/png")
+	public byte[] saveScreenshotPNG (WebDriver driver ){
+		
+		return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES)	;
+				
+	
+	}
+	
+	
+	@Attachment (value="{0}", type="text/plain")
+	public static String  saveTextLog(String message){
+		return message;
+	}
+
+	@Override
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
